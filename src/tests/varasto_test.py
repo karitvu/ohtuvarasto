d26a503e79b83422bc75a805a0c3e8fdc6b87788
karitvu <topi.val.kari@student.jyu.fi>
2025-10-30 20:27:43 +0200

    Testit tehty

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,37 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+        
+    def test_tehdaan_virheellisen_kokoinen(self):
+        self.varasto = Varasto(-1)
+        # varaston tilavuuden pitäisi olla 0
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+
+    def test_virheellinen_alkusaldo(self):
+        self.varasto = Varasto(10, -1)
+        # varastossa pitäisi olla tilaa 10
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisataan_negatiivista(self):
+        self.varasto.lisaa_varastoon(-1)
+        # varastossa pitäisi olla tilaa 10
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_lisataan_enemman_kuin_mahtuu(self):
+        self.varasto.lisaa_varastoon(11)
+        # varaston pitäisi olla täynnä
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    def test_otetaan_negatiivista(self):
+        # palauttaa 0
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-1), 0)
+
+    def test_otetaan_liikaa(self):
+        self.varasto.lisaa_varastoon(8)
+        # palauttaa kaiken mitä on, eli 8
+        self.assertAlmostEqual(self.varasto.ota_varastosta(10), 8)
+
+    def test_teksti(self):
+        testi = Varasto(10, 2)
+        # palauttaa tekstimuotoisen informaation
+        self.assertEqual(str(testi), "saldo = 2, vielä tilaa 8")
